03ef15102db1edcb2c0e66ca25418b4614c3b9ae
eakudompong.chanoknan <ceguantz96@outlook.com>
2026-04-11 11:18:11 +0900

    telescope

--- home/.config/nvim/lua/0xec/plugins/telescope.lua
+++ home/.config/nvim/lua/0xec/plugins/telescope.lua
@@ -11,8 +11,33 @@ return {
   config = function()
     local telescope = require("telescope")
     local builtin = require("telescope.builtin")
+    local action_layout = require("telescope.actions.layout")
 
     telescope.setup({
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+          width = 0.95,
+          preview_width = 0.45,
+        },
+        mappings = {
+          i = {
+            ["<M-p>"] = action_layout.toggle_preview,
+          },
+          n = {
+            ["<M-p>"] = action_layout.toggle_preview,
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          previewer = false,
+          path_display = { "truncate" },
+        },
+        live_grep = {
+          path_display = { "truncate" },
+        },
+      },
       extensions = {
         fzf = {
           fuzzy = true,
@@ -25,8 +50,12 @@ return {
 
     pcall(telescope.load_extension, "fzf")
 
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+    vim.keymap.set("n", "<leader>ff", function()
+      builtin.find_files()
+    end, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>fg", function()
+      builtin.live_grep()
+    end, { desc = "Live grep" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
   end,
