86bc0e57f0123bcacea1a91c349fe904c551c3e6
eakudompong.chanoknan <ceguantz96@outlook.com>
2026-04-25 14:43:32 +0900

    gitsign diffview snacks lazygit

---
+++ home/.config/nvim/lua/0xec/plugins/diffview.lua
@@ -0,0 +1,12 @@
+return {
+	"sindrets/diffview.nvim",
+	cmd = {
+		"DiffviewClose",
+		"DiffviewFileHistory",
+		"DiffviewOpen",
+		"DiffviewRefresh",
+		"DiffviewToggleFiles",
+	},
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {},
+}
